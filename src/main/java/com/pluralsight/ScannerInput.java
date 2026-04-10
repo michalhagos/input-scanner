a6d7c01c33735cdb9e6e49b89a1805909cc65785
michalhagos <mhayele3@gmail.com>
2026-04-09 21:45:42 -0500

    asked input from the user . declared a variable to hold the input. printed the input in complete sentences to the screen

--- src/main/java/com/pluralsight/ScannerInput.java
+++ src/main/java/com/pluralsight/ScannerInput.java
@@ -9,11 +9,21 @@ public class ScannerInput {
         Scanner michalScanner = new Scanner(System.in);
         // ask the user for input
       System.out.print("Enter your name here : ");
+      // store it in a variable in the next line
       String name = michalScanner.nextLine();
-      // print
+      // print the input and any other strings concatenated
       System.out.println("Hello " + name + " you got this keep going .");
 
-        //System.out.print
+      System.out.print("What is your favorite color? ");
+      String favColor = michalScanner.nextLine();
+        System.out.println("Your Favorite color is " + favColor);
+
+        System.out.print("Do you have any brothers ? ");
+        String brothers = michalScanner.nextLine();
+        System.out.println("Do you have brothers? " + brothers);
+
+
+
 
 
 
